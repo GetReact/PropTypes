32f85910537189e5cfbd563988bd49ff852249a8
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2020-04-12 23:45:48 +0100

    isRequired and defaultProps

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 import './App.css';
 import { Component } from 'react';
 import PropTypes from 'prop-types'
-const Person = ({ img, name, age }) => {
+const Person = ({ img, name, age, info }) => {
   return (
     <article>
       <img src={img} alt="person" />
       <h4>name : {name}</h4>
       <h4>age : {age}</h4>
+      <h4>info : {info}</h4>
     </article>
   )
 }
 Person.propTypes = {
-  img: PropTypes.string,
-  name: PropTypes.string,
-  age: PropTypes.number,
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+};
+Person.defaultProps = {
+  img: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
+  name: 'Bob',
+  age: 12,
+  info: 'default info about the person'
 }
 class PersonList extends Component {
   state = {
@@ -27,9 +34,16 @@ class PersonList extends Component {
       },
       {
         id: 2,
-        img: "https://randomuser.me/api/portraits/thumb/men/66.jpg",
-        name: 'Grace',
-        age: 29
+        // img: "https://randomuser.me/api/portraits/thumb/men/66.jpg",
+        // name: 'Grace',
+        // age: 29
+      },
+      {
+        id: 3,
+        img: "https://randomuser.me/api/portraits/thumb/men/88.jpg",
+        name: 'Peter',
+        age: 27,
+        info: 'baaa aaaaa aaaaaaaaaaa aaaa aaaaa nn nnnnn nnnnnn'
       }
     ]
   }
@@ -39,7 +53,12 @@ class PersonList extends Component {
         {
           this.state.people.map(
             (person) => (
-              <Person key={person.id} img={person.img} name={person.name} age={person.age}></Person>
+              <Person key={person.id}
+                img={person.img}
+                name={person.name}
+                age={person.age}>
+                info={person.info}>
+              </Person>
             )
           )
         }
