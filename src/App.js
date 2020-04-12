e8291bbc3b15e35711a3417c911091dd4931bcc8
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2020-04-12 23:28:50 +0100

    not first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { Component } from 'react';
+import PropTypes from 'prop-types'
+const Person = ({ img, name, age }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <article>
+      <img src={img} alt="person" />
+      <h4>name : {name}</h4>
+      <h4>age : {age}</h4>
+    </article>
+  )
 }
+Person.propTypes = {
+  img: PropTypes.string,
+  name: PropTypes.string,
+  age: PropTypes.number,
+}
+class PersonList extends Component {
+  state = {
+    people: [
+      {
+        id: 1,
+        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg",
+        name: 'Daniel',
+        age: 15
+      },
+      {
+        id: 2,
+        img: "https://randomuser.me/api/portraits/thumb/men/66.jpg",
+        name: 'Grace',
+        age: 29
+      }
+    ]
+  }
+  render() {
+    return (
+      <section>
+        {
+          this.state.people.map(
+            (person) => (
+              <Person key={person.id} img={person.img} name={person.name} age={person.age}></Person>
+            )
+          )
+        }
+      </section>
+    )
+  }
+}
+
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <PersonList />
+      </div>
+    )
+  }
+}
+
 
 export default App;
